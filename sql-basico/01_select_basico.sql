cfd2cb42f69ae560040c952e44491c6b2455b93d
Rayane Garcia <rayane.rg@gmail.com>
2025-07-14 10:48:32 -0300

    Create 01_select_basico.sql

---
+++ sql-basico/01_select_basico.sql
@@ -0,0 +1,8 @@
+-- 01_select_basico.sql
+-- Primeiros comandos SELECT
+
+-- Selecionando todas as colunas de uma tabela fictícia de clientes
+SELECT * FROM clientes;
+
+-- Selecionando colunas específicas
+SELECT nome, idade, cidade FROM clientes;

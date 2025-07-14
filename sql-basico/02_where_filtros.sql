eb5679912b76ab62f100e3366f64d3d0d155f103
Rayane Garcia <rayane.rg@gmail.com>
2025-07-14 10:48:52 -0300

    Create 02_where_filtros.sql

---
+++ sql-basico/02_where_filtros.sql
@@ -0,0 +1,8 @@
+-- 02_where_filtros.sql
+-- Consultas com WHERE
+
+-- Clientes com idade acima de 30
+SELECT * FROM clientes WHERE idade > 30;
+
+-- Produtos com preço menor que 50
+SELECT * FROM produtos WHERE preco < 50;
